6ae889c719652d7a8d62736e8f101f69c5a8f1dd
iredmon28 <i.redomon28@gmail.com>
2026-05-07 13:20:21 +0200

    Subida inicial de la clase Java
    una simple subida , nada mas. Código cortito y al pie  jeje

---
+++ teclado.java
@@ -0,0 +1,60 @@
+
+*/
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class teclado{
+    public static void main(String [] args){
+        ArrayList<Character> palabra = new ArrayList<>();
+        Scanner sc = new Scanner (System.in);
+        boolean fin = false;
+        
+        while( fin == false){
+            System.out.println("introduce el numero");
+            String combinacion = sc.nextLine();
+             if(combinacion.equals("fin")){
+                fin = true;
+                break; 
+            }
+            int [] transformado = pasarNumerico(combinacion);
+            char letra = interaccionTeclado(transformado);
+            System.out.println(letra);
+            palabra.add(letra);
+           
+        }
+         System.out.println(palabra);
+    }
+       
+    
+
+
+    public static  char interaccionTeclado(int[] numeros){
+       char[][] teclado = {
+        {'A', 'B', 'C'},
+        {'D', 'E', 'F'},
+        {'G', 'H', 'I'},
+        {'J', 'K', 'L'},
+        {'M', 'N', 'O'},
+        {'P', 'R', 'S'},
+        {'T', 'U', 'V'},
+        {'W', 'X', 'Y', 'Z'}
+        };
+        int primerNumero = numeros[0];
+        char letra = teclado[primerNumero-1][numeros.length - 1];
+        return letra;
+        
+    
+    
+
+
+    }
+
+    public  static int[] pasarNumerico(String combinación){
+         String [] numeros = combinación.split(",");
+        int [] array = new int[numeros.length];
+        for( int i = 0 ; i < numeros.length ; i++){
+            array[i] = Integer.parseInt(numeros[i]);
+        }
+        return array;
+    }
+}
